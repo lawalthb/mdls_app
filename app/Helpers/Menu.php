c06addc1f853eb0dadcfff271cf4c300a73ee984
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-10-13 12:10:11 +0100

    menus set

--- app/Helpers/Menu.php
+++ app/Helpers/Menu.php
@@ -7,261 +7,175 @@
 		[
 			'path' => 'home',
 			'label' => "Dashboard", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'icon' => '<i class="material-icons ">dashboard</i>'
 		],
 		
 		[
-			'path' => 'admins',
-			'label' => "Admins", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => '',
+			'label' => "Student Management", 
+			'icon' => '<i class="material-icons ">supervisor_account</i>','submenu' => [
+		[
+			'path' => 'users',
+			'label' => "Students", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		],
 		
 		[
-			'path' => 'appsettings',
-			'label' => "App Settings", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => 'users/add',
+			'label' => "Admission", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => "Bulk Adminssion", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'parents',
+			'label' => "Parents", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => "Banned Students", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		]
+	]
+		],
+		
+		[
+			'path' => '',
+			'label' => "Academics", 
+			'icon' => '<i class="material-icons ">class</i>','submenu' => [
+		[
+			'path' => 'subjects',
+			'label' => "Subjects", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		],
 		
 		[
 			'path' => 'classes',
 			'label' => "Classes", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'examsettings',
-			'label' => "Exam Settings", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'examsheetperformances',
-			'label' => "Exam Sheet Performances", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'examsheets',
-			'label' => "Exam Sheets", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'grades',
-			'label' => "Grades", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'plans',
-			'label' => "Plans", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'sessions',
-			'label' => "Sessions", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'subjects',
-			'label' => "Subjects", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		],
 		
 		[
 			'path' => 'terms',
 			'label' => "Terms", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		],
 		
 		[
-			'path' => 'transactions',
-			'label' => "Transactions", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => 'sessions',
+			'label' => "Sessions", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => "Assign Class Teacher", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => "Promote Students", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		]
+	]
+		],
+		
+		[
+			'path' => '',
+			'label' => "Examination", 
+			'icon' => '<i class="material-icons ">library_books</i>','submenu' => [
+		[
+			'path' => 'grades',
+			'label' => "Grade System", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => "Assign Score to Student", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => '',
+			'label' => "Assign Score to Subject", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'examsheets',
+			'label' => "Terminal Sheet", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'examsettings',
+			'label' => "Exam Settings", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		],
+		
+		[
+			'path' => 'examsheetperformances',
+			'label' => "Exam Sheet Performances", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
+		]
+	]
+		],
+		
+		[
+			'path' => '',
+			'label' => "Staff Management", 
+			'icon' => '<i class="material-icons ">account_circle</i>','submenu' => [
+		[
+			'path' => 'users/add',
+			'label' => "Add Staff", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		],
 		
 		[
 			'path' => 'users',
-			'label' => "Users", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'label' => "Bans", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		],
 		
 		[
-			'path' => 'webabouts',
-			'label' => "Web Abouts", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webbenefits',
-			'label' => "Web Benefits", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webblogcategories',
-			'label' => "Web Blog Categories", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webblogs',
-			'label' => "Web Blogs", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webcolours',
-			'label' => "Web Colours", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webcontacts',
-			'label' => "Web Contacts", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webcounters',
-			'label' => "Web Counters", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webctas',
-			'label' => "Web Ctas", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webevents',
-			'label' => "Web Events", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webexcos',
-			'label' => "Web Excos", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webgalleries',
-			'label' => "Web Galleries", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webheaders',
-			'label' => "Web Headers", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webregistrations',
-			'label' => "Web Registrations", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webresources',
-			'label' => "Web Resources", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'websettings',
-			'label' => "Web Settings", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'websliders',
-			'label' => "Web Sliders", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webtestimonials',
-			'label' => "Web Testimonials", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webtopbars',
-			'label' => "Web Topbars", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'webvissions',
-			'label' => "Web Vissions", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'studentdetails',
-			'label' => "Student Details", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'parents',
-			'label' => "Parents", 
-			'icon' => '<i class="material-icons">extension</i>'
-		],
-		
-		[
-			'path' => 'parentables',
-			'label' => "Parentables", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => '',
+			'label' => "Reset Password", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_right</i>'
 		]
-	] ;
-	}
-	
-	public static function navbartopleft(){
-		return [
-		[
-			'path' => 'studentdetails',
-			'label' => "Student Details", 
-			'icon' => '<i class="material-icons">extension</i>'
+	]
 		],
 		
 		[
-			'path' => 'parents',
-			'label' => "Parents", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => '',
+			'label' => "Website Management", 
+			'icon' => '<i class="material-icons ">web</i>'
 		],
 		
 		[
-			'path' => 'parentables',
-			'label' => "Parentables", 
-			'icon' => '<i class="material-icons">extension</i>'
-		]
-	] ;
-	}
-	
-	public static function navbartopright(){
-		return [
-		[
-			'path' => 'studentdetails',
-			'label' => "Student Details", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => 'appsettings',
+			'label' => "App Settings", 
+			'icon' => '<i class="material-icons ">settings_applications</i>'
 		],
 		
 		[
-			'path' => 'parents',
-			'label' => "Parents", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => '',
+			'label' => "Profile", 
+			'icon' => '<i class="material-icons ">person</i>'
 		],
 		
 		[
-			'path' => 'parentables',
-			'label' => "Parentables", 
-			'icon' => '<i class="material-icons">extension</i>'
+			'path' => 'menu9',
+			'label' => "LogOut", 
+			'icon' => '<i class="material-icons ">subdirectory_arrow_left</i>'
 		]
 	] ;
 	}
